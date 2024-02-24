8620275f271b0a3cb71a90bb2ffd783c09117b8a
Ali Aalam <aliaalam.me@gmail.com>
2024-02-24 18:58:27 +0500

    aa

--- src/Components/SignupModal.jsx
+++ src/Components/SignupModal.jsx
@@ -35,7 +35,7 @@ const SignupModal = ({ onClose }) => {
 
 
   return (
-    <div className="absolute py-4 top-0 left-0 w-full flex items-center justify-center bg-gray-800 bg-opacity-50 px-4 lg:h-full overflow-x-auto overflow-y-auto">
+    <div className="absolute py-8 top-0 left-0 w-full flex items-center justify-center bg-gray-800 bg-opacity-50 px-4  overflow-x-auto overflow-y-auto">
       <div className=" backdrop-blur-lg bg-[#333] bg-opacity-60  w-[1150px]   p-8 rounded shadow-lg px-4 ">
         <div className='flex justify-center text-white border-b-2 '>
           <h2 className="text-2xl font-bold mb-4"> Welcome Step {step}/{totalSteps}</h2>
