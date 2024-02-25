8baa97a60ccc98cafc3b3c7a978a56692968d79b
Ali Aalam <aliaalam.me@gmail.com>
2024-02-25 18:47:06 +0500

    ss

--- src/Components/SignupModal.jsx
+++ src/Components/SignupModal.jsx
@@ -334,7 +334,7 @@ const StepOne = ({ nextStep }) => {
   <input
     type="date"
     className="w-full bg-[#3c3c3c] placeholder-white hover:border-gray-300 rounded-md border-2 border-[#555] text-white py-2 px-4 focus:outline-none"
-    style={{ color: 'white', WebkitTextFillColor: 'white' }} // Set text color to white for iOS
+    style={{ color: 'white', WebkitTextFillColor: 'white', width: '100%', height: '40px' }} // Set text color to white for iOS and adjust width and height
   />
 
   {/* Style the date picker icon */}
@@ -347,6 +347,7 @@ const StepOne = ({ nextStep }) => {
     `}
   </style>
 </div>
+
 
 
 
